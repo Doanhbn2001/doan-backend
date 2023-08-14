796117719602b6b7c535d7716126fad57c32b304
Nguyen Hong Doanh <doanh2001bn@gmail.com>
2023-08-14 21:20:45 +0700

    deloy19

--- index.js
+++ index.js
@@ -13,8 +13,8 @@ const MONGODB_URI =
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-// app.set('trust proxy', 1);
-app.use(cors());
+app.set('trust proxy', 1);
+// app.use(cors());
 app.use(
   cors({
     // origin: '*',
